74b5771f282f4ace5e93efda7bd47a944cc65c8b
Huriel Lopes <huriellopes1996@gmail.com>
2018-08-19 23:35:20 -0300

    First Commit - Backup no MySQL

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 define("HOST", 'localhost'); // nome do servidor ou IP
 define("PORT", '3307'); // Porta
-define("DB", 'biblioteca'); // Banco que será feito o backup
-define("USER", 'root'); // Usuário
-define("PASS", '299610'); // Senha
+define("DB", ''); // Banco que será feito o backup
+define("USER", ''); // Usuário
+define("PASS", ''); // Senha
 define("CHARSET", 'utf-8'); // Charset
 define("BinMySQL", 'C:/Mysql5/bin/'); // Caminho do MySQL
 define("Destino", 'C:/backup/'); // Destino que será salvo
